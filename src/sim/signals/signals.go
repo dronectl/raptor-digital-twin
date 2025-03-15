e0fccccfe15c768cf43b7990b807fcbfd04427cd
ztnel <christian911@sympatico.ca>
2025-03-15 19:41:52 -0400

    use more idiomatic golang composition to make api more intuitive

--- src/sim/signals/signals.go
+++ src/sim/signals/signals.go
@@ -5,26 +5,40 @@ import (
 	"math/rand"
 )
 
-type SignalProcessor interface {
-    compute() float64
+type Signal interface {
+    Process() float64
+    Enable(enable bool)
 }
 
 type NoiseParams struct {
-    Enable bool
     Gain float64
     Offset float64
 }
 
-type Signal struct {
-    np NoiseParams
+type BaseSignal struct {
+    Noise NoiseParams
     out float64
     t float64
     UpdateFreq int // Hz
-    Enable bool
+    enable bool
 }
 
+// superimpose noise on output signal
+func (s *BaseSignal)applyNoise() {
+    // range [-1, 1]
+    gainSeed := rand.Float64() - rand.Float64()
+    s.out += s.Noise.Offset + (s.Noise.Gain * gainSeed)
+}
+
+func (s *BaseSignal)Enable(enable bool) {
+    s.t = 0.0
+    s.out = 0.0
+    s.enable = enable
+}
+
+// Sine Signal Generator
 type SignalSin struct {
-    signal Signal
+    BaseSignal
 
     Amplitude float64
     Offset float64
@@ -32,49 +46,73 @@ type SignalSin struct {
     Phase float64 // rads
 }
 
+// compute next sine signal valuu
+func (s *SignalSin)compute() {
+    if !s.enable {
+        return
+    }
+    s.t += 1.0 / float64(s.UpdateFreq)
+    s.out = s.Amplitude * math.Sin((2 * math.Pi * s.Frequency * s.t) + s.Phase) + s.Offset
+}
+
+func (s *SignalSin)Process() float64 {
+    s.compute()
+	s.applyNoise()
+    return s.out
+}
+
+func NewSignalSin(amplitude, offset, freq, phase float64, updateFreq int) *SignalSin {
+    return &SignalSin{
+        BaseSignal: BaseSignal{
+            Noise: NoiseParams{Offset: 0.0, Gain: 0.0},
+            out: 0.0,
+            t: 0.0,
+            enable: false,
+            UpdateFreq: updateFreq,
+        },
+        Amplitude: amplitude,
+        Offset: offset,
+        Frequency: freq,
+        Phase: phase,
+    }
+}
+
+// Exponential Signal Generator
 type SignalExp struct {
-    signal Signal
+    BaseSignal
 
     // ramp rate
     Tau float64
     Ref float64
 }
 
-// superimpose noise on output signal
-func (s *Signal)noiseGen() {
-    if s.np.Enable {
-        // range [-1, 1]
-        gainSeed := rand.Float64() - rand.Float64()
-        s.out += s.np.Offset + (s.np.Gain * gainSeed)
-    }
-}
-
-func (s *Signal)Reset() {
-    s.t = 0.0
-}
-
-// compute next sine signal valuu
-func (s *SignalSin)compute() float64 {
-    s.signal.t += 1.0 / float64(s.signal.UpdateFreq)
-    s.signal.out = s.Amplitude * math.Sin((2 * math.Pi * s.Frequency * s.signal.t) + s.Phase) + s.Offset
-    return s.signal.out
-}
-
 // compute next exponential signal valuu
-func (s *SignalExp)compute() float64 {
-    s.signal.t += 1.0 / float64(s.signal.UpdateFreq)
-    s.signal.out = (s.Ref - s.signal.out)*(1.0 - math.Exp(-s.signal.t / s.Tau)) + s.signal.out
-    return s.signal.out
+func (s *SignalExp)compute() {
+    if !s.enable {
+        return
+    }
+    s.t += 1.0 / float64(s.UpdateFreq)
+    s.out = (s.Ref - s.out)*(1.0 - math.Exp(-s.t / s.Tau)) + s.out
 }
 
-// Process the signal with thread safety
-func Process(sp SignalProcessor, signal *Signal) float64 {
-    if signal.Enable {
-        signal.out = sp.compute()
-    } else {
-        signal.out = 0.0
+func (s *SignalExp)Process() float64 {
+    s.compute()
+	s.applyNoise()
+    return s.out
+}
+
+
+func NewSignalExp(tau float64, updateFreq int) *SignalExp {
+    return &SignalExp{
+        BaseSignal: BaseSignal{
+            Noise: NoiseParams{Offset: 0.0, Gain: 0.0},
+            out: 0.0,
+            t: 0.0,
+            enable: false,
+            UpdateFreq: updateFreq,
+        },
+        Tau: tau,
+        Ref: 0.0,
     }
-	signal.noiseGen()
-	return signal.out
 }
 
--- src/sim/signals/signals_test.go
+++ src/sim/signals/signals_test.go
@@ -12,32 +12,22 @@ func TestSignalProcessExp(t *testing.T) {
         offset float64 = 0.001
         gain float64 = 1.1
     )
-    exp := &SignalExp{
-        signal: Signal{
-            np: NoiseParams{
-                Enable: true,
-                Offset: offset,
-                Gain: gain,
-            },
-            out: initial,
-            Enable: true,
-            UpdateFreq: 1000,
-        },
-        Tau: tau,
-    }
+    exp := NewSignalExp(tau, 1000)
+    exp.Noise.Offset = offset
+    exp.Noise.Gain = gain
     for _, ref := range []float64{10.0, 0.0} {
         t.Logf("Signal Reference Step %f -> %f", exp.Ref, ref)
         exp.Ref = ref
-        exp.signal.Reset()
+        exp.Enable(true)
         for range 10 {
             // run process
-            v := Process(exp, &exp.signal)
+            v := exp.Process()
             // calculate expected signal value
-            base := (exp.Ref - exp.signal.out)*(1.0 - math.Exp(-exp.signal.t/ exp.Tau)) + exp.signal.out
-            noiseLower := -exp.signal.np.Gain + exp.signal.np.Offset + base
-            noiseUpper := exp.signal.np.Gain + exp.signal.np.Offset + base
-            if v != exp.signal.out {
-                t.Errorf("Expected %f got %f", exp.signal.out, v)
+            base := (exp.Ref - exp.out)*(1.0 - math.Exp(-exp.t/ exp.Tau)) + exp.out
+            noiseLower := -exp.Noise.Gain + exp.Noise.Offset + base
+            noiseUpper := exp.Noise.Gain + exp.Noise.Offset + base
+            if v != exp.out {
+                t.Errorf("Expected %f got %f", exp.out, v)
                 break
             }
             t.Logf("signal: %f expected: %f, range: [%f, %f]", v, base, noiseLower, noiseUpper)
@@ -55,30 +45,20 @@ func TestSignalProcessSin(t *testing.T) {
         amplitude float64 = 12.0
         offset float64 = 12.0
         phase float64 = 0.0 
-        frequency float64 = 1000.0
+        frequency float64 = 10.0
         noiseGain float64 = 1.0
         noiseOffset float64 = 0.1
     ) 
-    sin := &SignalSin{
-        signal: Signal{
-            np: NoiseParams{Enable: true, Offset: noiseOffset, Gain: noiseGain},
-            out: initial,
-            UpdateFreq: 10000,
-            Enable: true,
-        },
-        Amplitude: amplitude,
-        Offset: offset,
-        Frequency: frequency,
-        Phase: phase,
-    }
+    sin := NewSignalSin(amplitude, offset, frequency, phase, 1000)
+    sin.Enable(true)
     for range 10 {
         // run process
-        v := Process(sin, &sin.signal)
-        base := sin.Amplitude * math.Sin((math.Pi * 2 * sin.Frequency * sin.signal.t) + sin.Phase) + sin.Offset
-        noiseLower := -sin.signal.np.Gain + sin.signal.np.Offset + base
-        noiseUpper := sin.signal.np.Gain + sin.signal.np.Offset + base
-        if v != sin.signal.out {
-            t.Errorf("Expected %f got %f", sin.signal.out, v)
+        v := sin.Process()
+        base := sin.Amplitude * math.Sin((math.Pi * 2 * sin.Frequency * sin.t) + sin.Phase) + sin.Offset
+        noiseLower := -sin.Noise.Gain + sin.Noise.Offset + base
+        noiseUpper := sin.Noise.Gain + sin.Noise.Offset + base
+        if v != sin.out {
+            t.Errorf("Expected %f got %f", sin.out, v)
             break
         }
         t.Logf("signal: %f expected: %f, range: [%f, %f]", v, base, noiseLower, noiseUpper)
